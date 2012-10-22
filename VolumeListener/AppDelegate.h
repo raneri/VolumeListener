2497e4f9351191c49830047fb1a5f719621cbdd8
Riccardo Raneri <ricky@MacBook-Pro-di-Riccardo.local>
2012-10-22 12:49:33 +0200

    Initial Commit

---
+++ VolumeListener/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VolumeListener
+//
+//  Created by Riccardo Raneri on 22/10/12.
+//  Copyright (c) 2012 Riccardo Raneri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VolumeListener/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VolumeListener
+//
+//  Created by Riccardo Raneri on 22/10/12.
+//  Copyright (c) 2012 Riccardo Raneri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
